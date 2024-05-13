cf79085a0e7937ce799701dde97e22a300ef8bc9
afiqnexea <afiqnrzm@hotmail.com>
2024-05-14 04:40:44 +0800

    a

--- src/sections/admin/dashboard.jsx
+++ src/sections/admin/dashboard.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { lazy } from 'react';
 
-import { Container } from '@mui/material';
+import { Box, Grid, alpha, Container } from '@mui/material';
 
 import { useAuthContext } from 'src/auth/hooks';
 
@@ -17,6 +17,35 @@ const DashboardAdmin = () => {
   return (
     <Container maxWidth={settings.themeStretch ? false : 'lg'}>
       {user?.admin?.designation === 'Finance' && <DashboardFinance />}
+      {user?.admin?.designation === 'CSM' && (
+        <Grid container columnSpacing={5}>
+          {JSON.stringify(user)}
+          <Grid item xs={12} md={8}>
+            <Box
+              sx={{
+                mt: 5,
+                width: 1,
+                height: 320,
+                borderRadius: 2,
+                bgcolor: (theme) => alpha(theme.palette.grey[500], 0.04),
+                border: (theme) => `dashed 1px ${theme.palette.divider}`,
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box
+              sx={{
+                mt: 5,
+                width: 1,
+                height: 320,
+                borderRadius: 2,
+                bgcolor: (theme) => alpha(theme.palette.grey[500], 0.04),
+                border: (theme) => `dashed 1px ${theme.palette.divider}`,
+              }}
+            />
+          </Grid>
+        </Grid>
+      )}
       {/* <Grid container columnSpacing={5}>
         {JSON.stringify(user)}
         <Grid item xs={12} md={8}>
