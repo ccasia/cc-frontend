737694059bdd36beaa4fcda88e3137a0a520722a
afiqnexea <afiqnrzm@hotmail.com>
2024-06-24 14:56:13 +0800

    a

--- src/sections/campaign/discover/admin/campaign-detail-pitch.jsx
+++ src/sections/campaign/discover/admin/campaign-detail-pitch.jsx
@@ -1,57 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import dayjs from 'dayjs';
 import PropTypes from 'prop-types';
 
-import { Box, Card, Chip, Stack, IconButton, ListItemText } from '@mui/material';
+import {
+  Box,
+  Card,
+  Chip,
+  Stack,
+  IconButton,
+  ListItemText,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+} from '@mui/material';
 
 import Image from 'src/components/image';
 import Iconify from 'src/components/iconify';
+import Markdown from 'src/components/markdown';
 
-const CampaignDetailPitch = ({ pitches }) => (
-  <Box display="grid" gridTemplateColumns="repeat(3,1fr)" gap={2}>
-    {pitches &&
-      pitches.map((pitch) => (
-        <>
-          {/* {JSON.stringify(pitch)} */}
-          <Card
-            sx={{
-              p: 1.5,
-            }}
-          >
-            <IconButton
+const CampaignDetailPitch = ({ pitches }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box display="grid" gridTemplateColumns="repeat(3,1fr)" gap={2}>
+      {pitches &&
+        pitches.map((pitch) => (
+          <>
+            <Card
               sx={{
-                position: ' absolute',
-                top: 0,
-                right: 0,
+                p: 1.5,
               }}
             >
-              <Iconify icon="fluent:open-12-filled" width={16} />
-            </IconButton>
-            <Stack direction="row" spacing={2}>
-              <Image
-                src="/test.jpeg"
-                ratio="1/1"
+              <IconButton
                 sx={{
-                  borderRadius: 1,
-                  width: 70,
+                  position: ' absolute',
+                  top: 0,
+                  right: 0,
                 }}
-              />
-              <Stack spacing={1} alignItems="start">
-                <ListItemText
-                  primary={pitch?.user?.name}
-                  secondary={`Pitch at ${dayjs(pitch?.createdAt).format('LL')}`}
-                  primaryTypographyProps={{
-                    typography: 'subtitle1',
+                onClick={() => setOpen(true)}
+              >
+                <Iconify icon="fluent:open-12-filled" width={16} />
+              </IconButton>
+              <Stack direction="row" spacing={2}>
+                <Image
+                  src="/test.jpeg"
+                  ratio="1/1"
+                  sx={{
+                    borderRadius: 1,
+                    width: 70,
                   }}
                 />
-                <Chip label={pitch?.type} size="small" color="secondary" />
+                <Stack spacing={1} alignItems="start">
+                  <ListItemText
+                    primary={pitch?.user?.name}
+                    secondary={`Pitch at ${dayjs(pitch?.createdAt).format('LL')}`}
+                    primaryTypographyProps={{
+                      typography: 'subtitle1',
+                    }}
+                  />
+                  <Chip label={pitch?.type} size="small" color="secondary" />
+                </Stack>
               </Stack>
-            </Stack>
-          </Card>
-        </>
-      ))}
-  </Box>
-);
+            </Card>
+            <Dialog open={open} onClose={handleClose}>
+              <DialogTitle>Pitch Content</DialogTitle>
+              <DialogContent>
+                <Markdown children={pitch?.content} />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Close</Button>
+              </DialogActions>
+            </Dialog>
+          </>
+        ))}
+    </Box>
+  );
+};
 
 export default CampaignDetailPitch;
 
--- src/sections/campaign/discover/creator/campaign-modal.jsx
+++ src/sections/campaign/discover/creator/campaign-modal.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import * as Yup from 'yup';
 import PropTypes from 'prop-types';
 import 'react-quill/dist/quill.snow.css';
 import { useForm } from 'react-hook-form';
 import { enqueueSnackbar } from 'notistack';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import {
   Box,
@@ -23,9 +25,6 @@ import Iconify from 'src/components/iconify';
 import { RHFEditor } from 'src/components/hook-form';
 import FormProvider from 'src/components/hook-form/form-provider';
 
-import * as Yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
-
 const CampaignModal = ({ open, handleClose, campaign }) => {
   const schema = Yup.object().shape({
     content: Yup.string().required('Pitch Script is required'),
@@ -41,13 +40,11 @@ const CampaignModal = ({ open, handleClose, campaign }) => {
   const { handleSubmit } = methods;
 
   const onSubmit = handleSubmit(async (data) => {
-    alert(JSON.stringify(data));
-  });
-
-  const pitch = async () => {
     try {
-      onSubmit();
-      const res = await axiosInstance.patch(endpoints.campaign.pitch, { campaignId: campaign?.id });
+      const res = await axiosInstance.patch(endpoints.campaign.pitch, {
+        campaignId: campaign?.id,
+        ...data,
+      });
       enqueueSnackbar(res?.data?.message);
       handleClose();
     } catch (error) {
@@ -55,7 +52,7 @@ const CampaignModal = ({ open, handleClose, campaign }) => {
         variant: 'error',
       });
     }
-  };
+  });
 
   return (
     <Dialog open={open} onClose={handleClose} fullWidth maxWidth="md">
