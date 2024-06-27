7ba4ad771312832103ab2ca951b708bfd6831ced
afiqnexea <afiqnrzm@hotmail.com>
2024-06-27 13:10:50 +0800

    a

--- src/sections/brand/details/brand/brand-general.jsx
+++ src/sections/brand/details/brand/brand-general.jsx
@@ -123,7 +123,7 @@ export default function BrandsList({ brands, onView, onEdit, onDelete }) {
                     />
 
                     <Typography variant="subtitle2" color={theme.palette.grey[400]}>
-                      {brand?.description.length > 50
+                      {brand?.description && brand?.description.length > 50
                         ? brand?.description.slice(0, 100).concat('...')
                         : brand?.description}
                     </Typography>
