5cbd70115b0f23a855e0f13f8436affbe088ee37
afiqnexea <afiqnrzm@hotmail.com>
2024-08-26 03:52:15 +0800

    A

--- src/sections/admin/dashboard.jsx
+++ src/sections/admin/dashboard.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { lazy } from 'react';
+import React from 'react';
 
 import { Container } from '@mui/material';
 
@@ -9,7 +9,7 @@ import { useSettingsContext } from 'src/components/settings';
 
 import DashboardSuperadmin from './dashboard-superadmin';
 
-const DashboardFinance = lazy(() => import('src/sections/admin/finance/Dashboard'));
+// const DashboardFinance = lazy(() => import('src/sections/admin/finance/Dashboard'));
 
 const DashboardAdmin = () => {
   const settings = useSettingsContext();
