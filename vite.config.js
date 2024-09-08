76211f0f90a1489b48df4714128c967b3ee5ce08
Fathul Amin <116142085+famintech@users.noreply.github.com>
2024-09-08 14:31:11 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -3,40 +3,48 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import checker from 'vite-plugin-checker';
 
-// ----------------------------------------------------------------------
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    checker({
-      eslint: {
-        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
-      },
-      overlay: {
-        initialIsOpen: false,
-      },
-    }),
-  ],
+  return {
+    plugins: [
+      react(),
+      !isProduction &&
+        checker({
+          eslint: {
+            lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
+          },
+          overlay: {
+            initialIsOpen: false,
+          },
+        }),
+    ].filter(Boolean),
 
-  resolve: {
-    alias: [
-      {
-        find: /^~(.+)/,
-        replacement: path.join(process.cwd(), 'node_modules/$1'),
-      },
-      {
-        find: /^src(.+)/,
-        replacement: path.join(process.cwd(), 'src/$1'),
-      },
-    ],
-  },
-  server: {
-    port: 3030,
-    strictPort: true,
-    host: true,
-    origin: 'http://0.0.0.0:3030',
-  },
-  preview: {
-    port: 3030,
-  },
+    resolve: {
+      alias: [
+        {
+          find: /^~(.+)/,
+          replacement: path.join(process.cwd(), 'node_modules/$1'),
+        },
+        {
+          find: /^src(.+)/,
+          replacement: path.join(process.cwd(), 'src/$1'),
+        },
+      ],
+    },
+    server: {
+      port: 3030,
+      strictPort: true,
+      host: true,
+      origin: 'http://0.0.0.0:3030',
+    },
+    preview: {
+      port: 3030,
+    },
+    build: {
+      // Add any production-specific build options here
+      sourcemap: false,
+      minify: 'terser',
+    },
+  };
 });
