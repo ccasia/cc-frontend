dee70d8866bdafb803ee7e6febb134efffebe726
Zawad Wasik Ahmed <zawadwasik@gmail.com>
2024-09-08 16:05:05 +0800

    Update

--- src/layouts/auth/creator.jsx
+++ src/layouts/auth/creator.jsx
@@ -48,7 +48,8 @@ export default function AuthModernLayout({ children, image }) {
       <Box
         component="img"
         alt="auth"
-        src={image || '/assets/images/login/creator.jpg'}
+        // src={image || '/assets/images/login/creator.jpg'} old image 
+        src={image || '/assets/images/login/cultimage.png'}
         sx={{
           top: 16,
           left: 16,
