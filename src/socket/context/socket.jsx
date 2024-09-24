4025e7dc32141cf70fca6fa2665ff4cddc0f6107
afiqnexea <afiqnrzm@hotmail.com>
2024-09-24 14:34:57 +0800

    a

--- src/socket/context/socket.jsx
+++ src/socket/context/socket.jsx
@@ -14,7 +14,9 @@ const SocketProvider = ({ children }) => {
   useEffect(() => {
     // I used this for my connection - Zawad
     //  const socketConnection = io({transports:['polling'],reconnect:true,path:'/api/socket.io'});
-    const socketConnection = io('http://localhost:3002');
+    const socketConnection = io('http://34.142.146.253:3002/', {
+      reconnection: true,
+    });
 
     socketConnection.on('connect', () => {
       console.log('Connected');
