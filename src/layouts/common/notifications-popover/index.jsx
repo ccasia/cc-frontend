75a13242aab99e0e3bdf037e7e13a8177950bcdb
afiqnexea <afiqnrzm@hotmail.com>
2024-10-15 17:56:25 +0800

    a

--- src/layouts/common/notifications-popover/index.jsx
+++ src/layouts/common/notifications-popover/index.jsx
@@ -7,7 +7,6 @@ import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import List from '@mui/material/List';
-import { alpha } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import Badge from '@mui/material/Badge';
 import Drawer from '@mui/material/Drawer';
@@ -129,25 +128,50 @@ export default function NotificationsPopover() {
       value={currentTab}
       onChange={handleChangeTab}
       variant="fullWidth"
-      sx={{
-        '& .MuiTabs-indicator': {
-          width: '100%',
-          height: '100%',
-          display: 'block',
-          borderRadius: 1,
+      TabIndicatorProps={{
+        children: <span />,
+        sx: {
+          height: 1,
+          py: 1,
           zIndex: -10000,
-          bgcolor: (theme) =>
-            theme.palette.mode === 'dark'
-              ? alpha(theme.palette.grey[900], 1)
-              : alpha(theme.palette.grey[300], 1),
+          bgcolor: 'transparent',
           transition: 'all .3s ease-in-out',
+          '&.MuiTabs-indicator > span': {
+            bgcolor: (theme) =>
+              theme.palette.mode === 'dark' ? theme.palette.grey[900] : theme.palette.grey[200],
+            width: '100%',
+            height: '100%',
+            display: 'block',
+            borderRadius: 1,
+            boxShadow: 2,
+          },
+        },
+      }}
+      sx={{
+        '&.MuiTabs-root': {
+          zIndex: 1,
+          minHeight: 'auto',
+          flexShrink: 0,
+        },
+        '& .MuiTabs-scroller': {
+          p: 1,
         },
       }}
     >
       {TABS.map((tab) => (
         <Tab
+          // style={{ backgroundColor: 'black', borderRadius: 5 }}
           key={tab.value}
           iconPosition="end"
+          sx={{
+            '&.Mui-selected': {
+              bgcolor: 'transparent',
+              fontWeight: 600,
+            },
+            '&:not(:last-of-type)': {
+              mr: 0,
+            },
+          }}
           value={tab.value}
           label={tab.label}
           icon={
@@ -166,11 +190,6 @@ export default function NotificationsPopover() {
                 data?.notifications.filter((notification) => notification.archive).length}
             </Label>
           }
-          // sx={{
-          //   '&:not(:last-of-type)': {
-          //     mr: 3,
-          //   },
-          // }}
         />
       ))}
     </Tabs>
@@ -237,14 +256,7 @@ export default function NotificationsPopover() {
 
         <Divider />
 
-        {/* <Stack
-          direction="row"
-          alignItems="center"
-          justifyContent="space-between"
-          // sx={{ pl: 2.5, pr: 1 }}
-        >
-        </Stack> */}
-        <Box sx={{ p: 0.5 }}>{renderTabs}</Box>
+        {renderTabs}
 
         <Divider />
 
