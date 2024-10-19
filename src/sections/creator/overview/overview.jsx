2af0a7642524e555ae41c6cf4458547d260a11cd
afiqnexea <afiqnrzm@hotmail.com>
2024-10-20 01:35:43 +0800

    a

--- src/sections/creator/overview/overview.jsx
+++ src/sections/creator/overview/overview.jsx
@@ -12,15 +12,19 @@ import {
   ListItemText,
 } from '@mui/material';
 
+import { paths } from 'src/routes/paths';
+import { RouterLink } from 'src/routes/components';
+
+import useGetOverview from 'src/hooks/use-get-overview';
+
 import { useAuthContext } from 'src/auth/hooks';
 
 import Image from 'src/components/image';
 import Iconify from 'src/components/iconify';
 
 const Overview = () => {
-  console.log('DASD');
-
   const { user } = useAuthContext();
+  const { data, isLoading } = useGetOverview();
 
   const renderOverview = (
     <Grid container mt={3} spacing={2}>
@@ -51,85 +55,74 @@ const Overview = () => {
               }}
             />
           </Stack>
+
           <Divider />
+
           <Stack
             spacing={1}
             sx={{
               p: 2,
             }}
           >
-            <Card sx={{ p: 2 }}>
-              <Stack direction="row" alignItems="center" justifyContent="space-between">
-                <Stack direction="row" spacing={2} alignItems="center">
-                  <Avatar>H</Avatar>
-                  <ListItemText
-                    primary="Sauz it"
-                    secondary="A&W"
-                    primaryTypographyProps={{
-                      variant: 'h6',
-                      fontWeight: 'bold',
-                      color: 'black',
-                    }}
-                    secondaryTypographyProps={{
-                      variant: 'subtitle2',
-                      color: 'text.secondary',
-                    }}
-                  />
-                </Stack>
-                <Typography variant="subtitle2" color="text.secondary">
-                  82% Completed
-                </Typography>
-              </Stack>
-              <Divider
-                sx={{
-                  my: 2,
-                }}
-              />
-              <Typography variant="subtitle2" color="text.secondary">
-                Last update: Awaiting client approval.
-              </Typography>
-            </Card>
-            <Card sx={{ p: 2 }}>
-              <Stack direction="row" alignItems="center" justifyContent="space-between">
-                <Stack direction="row" spacing={2} alignItems="center">
-                  <Avatar>H</Avatar>
-                  <ListItemText
-                    primary="Sauz it"
-                    secondary="A&W"
-                    primaryTypographyProps={{
-                      variant: 'h6',
-                      fontWeight: 'bold',
-                      color: 'black',
-                    }}
-                    secondaryTypographyProps={{
-                      variant: 'subtitle2',
-                      color: 'text.secondary',
+            {!isLoading &&
+              data.map((item, index) => (
+                <Card sx={{ p: 2 }} key={index}>
+                  <Stack direction="row" alignItems="center" justifyContent="space-between">
+                    <Stack direction="row" spacing={2} alignItems="center">
+                      <Avatar src={item?.campaignImages[0]}>H</Avatar>
+                      <ListItemText
+                        primary={item?.campaignName}
+                        secondary={item?.brand?.name}
+                        primaryTypographyProps={{
+                          variant: 'h6',
+                          fontWeight: 'bold',
+                          color: 'black',
+                        }}
+                        secondaryTypographyProps={{
+                          variant: 'subtitle2',
+                          color: 'text.secondary',
+                        }}
+                      />
+                    </Stack>
+                    <Typography variant="subtitle2" color="text.secondary">
+                      {item.completed}% Completed
+                    </Typography>
+                  </Stack>
+                  <Divider
+                    sx={{
+                      my: 2,
                     }}
                   />
-                </Stack>
-                <Typography variant="subtitle2" color="text.secondary">
-                  82% Completed
-                </Typography>
-              </Stack>
-              <Divider
-                sx={{
-                  my: 2,
-                }}
-              />
-              <Typography variant="subtitle2" color="text.secondary">
-                Last update: Awaiting client approval.
-              </Typography>
-            </Card>
+                  <Typography variant="subtitle2" color="text.secondary">
+                    Last update: Awaiting client approval.
+                  </Typography>
+                </Card>
+              ))}
           </Stack>
+
           <Divider />
+
           <Box sx={{ p: 2, display: 'flex', alignItems: 'center', gap: 1 }}>
-            <Typography variant="subtitle2" color="text.secondary" fontWeight="bolder">
+            <Typography
+              variant="subtitle2"
+              color="text.secondary"
+              fontWeight="bolder"
+              component={RouterLink}
+              sx={{
+                textDecoration: 'none',
+                '&:hover': {
+                  textDecoration: 'underline',
+                },
+              }}
+              href={paths.dashboard.campaign.creator.manage}
+            >
               SEE ALL CAMPAIGNS
             </Typography>
             <Iconify icon="ic:round-navigate-next" width={18} />
           </Box>
         </Card>
       </Grid>
+
       <Grid item xs={12} md={6}>
         <Card>
           <Stack
