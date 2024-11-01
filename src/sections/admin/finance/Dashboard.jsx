0d98dcf69c0c1377c377650a7897c65e16079548
afiqnexea <afiqnrzm@hotmail.com>
2024-11-01 14:43:43 +0800

    a

--- src/sections/admin/finance/Dashboard.jsx
+++ src/sections/admin/finance/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Grid, Container, Typography } from '@mui/material';
+import { Box, Grid, Container, Typography, CircularProgress } from '@mui/material';
 
 import { useGetAllInvoices } from 'src/api/invoices';
 
@@ -160,13 +160,24 @@ const Dashboard = () => {
             Invoice History.
           </Typography>
 
-          {!isLoading && (
+          {!isLoading ? (
             <InvoiceHistoryCampaignList
               data={filteredData}
               onDataUpdate={handleDataUpdate}
               searchQuery={searchQuery}
               onSearchChange={handleSearchChange}
             />
+          ) : (
+            <Box textAlign="center">
+              <CircularProgress
+                thickness={7}
+                size={25}
+                sx={{
+                  color: (theme) => theme.palette.common.black,
+                  strokeLinecap: 'round',
+                }}
+              />
+            </Box>
           )}
         </Grid>
       </Grid>
