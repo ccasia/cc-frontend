d90372f3bc70b2d7de6eaaf89fde0c47e8b84cca
Afiq Danial <afiq@nexea.co>
2024-11-15 11:15:47 +0800

    a

--- src/layouts/dashboard/index.jsx
+++ src/layouts/dashboard/index.jsx
@@ -12,14 +12,17 @@ import { endpoints } from 'src/utils/axios';
 import { useAuthContext } from 'src/auth/hooks';
 import useSocketContext from 'src/socket/hooks/useSocketContext';
 
+import { useSettingsContext } from 'src/components/settings';
+
 import Main from './main';
 import Header from './header';
+import NavMini from './nav-mini';
 import NavVertical from './nav-vertical';
 
 // ----------------------------------------------------------------------
 
 export default function DashboardLayout({ children }) {
-  // const settings = useSettingsContext();
+  const settings = useSettingsContext();
 
   const { user } = useAuthContext();
   const { socket, isOnline } = useSocketContext();
@@ -45,9 +48,9 @@ export default function DashboardLayout({ children }) {
 
   // const isHorizontal = settings.themeLayout === 'horizontal';
 
-  // const isMini = settings.themeLayout === 'mini';
+  const isMini = settings.themeLayout === 'mini';
 
-  // const renderNavMini = <NavMini />;
+  const renderNavMini = <NavMini />;
 
   // const renderHorizontal = <NavHorizontal />;
 
@@ -65,82 +68,38 @@ export default function DashboardLayout({ children }) {
   //   );
   // }
 
-  // if (isMini) {
-  //   return (
-  //     <>
-  //       {/* <Header onOpenNav={nav.onTrue} isOnline={isOnline} />
+  if (isMini) {
+    return (
+      <Box
+        sx={{
+          minHeight: 1,
+          display: 'flex',
+          flexDirection: { xs: 'column', lg: 'row' },
+          pr: lgUp && 2,
+        }}
+      >
+        {lgUp ? renderNavMini : renderNavVertical}
 
-  //       <Box
-  //         sx={{
-  //           minHeight: 1,
-  //           display: 'flex',
-  //           flexDirection: { xs: 'column', lg: 'row' },
-  //         }}
-  //       >
-  //         {lgUp ? renderNavMini : renderNavVertical}
+        <Box
+          sx={{
+            ...(lgUp && {
+              width: 1,
+              height: '95vh',
+              borderRadius: 2,
+              my: 'auto',
+              overflow: 'auto',
+              position: 'relative',
+              bgcolor: (theme) => theme.palette.background.paper,
+            }),
+          }}
+        >
+          <Header onOpenNav={nav.onTrue} isOnline={isOnline} />
 
-  //         <Main>{children}</Main>
-  //       </Box> */}
-  //       <Box
-  //         sx={{
-  //           minHeight: 1,
-  //           display: 'flex',
-  //           flexDirection: { xs: 'column', lg: 'row' },
-  //         }}
-  //       >
-  //         {lgUp ? renderNavMini : renderNavVertical}
-
-  //         {/* <Box
-  //           sx={{
-  //             flexGrow: 1,
-  //             bgcolor: (theme) => theme.palette.background.default,
-  //             height: '100vh',
-  //             width: '100%',
-  //             py: lgUp && 2,
-  //             pr: lgUp && 2,
-  //           }}
-  //         >
-  //           <Box
-  //             sx={{
-  //               bgcolor: (theme) => theme.palette.background.paper,
-  //               width: 1,
-  //               height: 1,
-  //               borderRadius: 2,
-  //               overflow: 'auto',
-  //             }}
-  //           >
-  //             <Header onOpenNav={nav.onTrue} isOnline={isOnline} />
-
-  //             <Main sx={{ mt: 2 }}>{children}</Main>
-  //           </Box>
-  //         </Box> */}
-  //         <Box
-  //           sx={{
-  //             width: 1,
-  //             height: '95vh',
-  //             borderRadius: 2,
-  //             my: 'auto',
-  //             overflow: 'hidden',
-  //             display: 'flex',
-  //             flexDirection: 'column',
-  //           }}
-  //         >
-  //           <Header onOpenNav={nav.onTrue} isOnline={isOnline} />
-
-  //           <Box
-  //             sx={{
-  //               scrollbarWidth: 'none',
-  //               overflow: 'auto',
-  //               height: 1,
-  //             }}
-  //           >
-  //             <Main sx={{ py: 2 }}>{children}</Main>
-  //           </Box>
-  //         </Box>
-  //       </Box>
-  //     </>
-  //   );
-  // }
+          <Main sx={{ py: 2 }}>{children}</Main>
+        </Box>
+      </Box>
+    );
+  }
 
   return (
     <Box
