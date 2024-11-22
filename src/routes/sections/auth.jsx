f0390a02c35b47afb4b219a41ee3bdec7ed4cc37
Afiq Danial <afiq@nexea.co>
2024-11-22 14:19:42 +0800

    admin login page redesign

--- src/routes/sections/auth.jsx
+++ src/routes/sections/auth.jsx
@@ -5,7 +5,6 @@ import { Outlet } from 'react-router-dom';
 import { GuestGuard } from 'src/auth/guard';
 import CompactLayout from 'src/layouts/compact';
 import AuthModernLayout from 'src/layouts/auth/creator';
-import AuthClassicLayout from 'src/layouts/auth/classic';
 
 import { SplashScreen } from 'src/components/loading-screen';
 
@@ -48,9 +47,9 @@ const authAdmin = {
           path: 'login',
           element: (
             <GuestGuard>
-              <AuthClassicLayout>
+              <AuthModernLayout>
                 <JwtLoginPage />
-              </AuthClassicLayout>
+              </AuthModernLayout>
             </GuestGuard>
           ),
         },
