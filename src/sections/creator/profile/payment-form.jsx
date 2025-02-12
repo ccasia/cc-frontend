4c44524f20a7582a86ee3204accc61ce2b6236cc
Afiq Danial <afiq@nexea.co>
2025-02-12 17:23:01 +0800

    added others field in bank name options

--- src/sections/creator/profile/payment-form.jsx
+++ src/sections/creator/profile/payment-form.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
 import * as yup from 'yup';
 import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
 import { enqueueSnackbar } from 'notistack';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 import { LoadingButton } from '@mui/lab';
-import { Box, Paper, TextField, Typography } from '@mui/material';
+import { Box, Paper, TextField, Typography, createFilterOptions } from '@mui/material';
 
 import { banks } from 'src/contants/bank';
 import { updatePaymentForm } from 'src/api/paymentForm';
@@ -23,6 +23,8 @@ const schema = yup.object().shape({
   bankAccName: yup.string().required('Bank account name is required'),
   icPassportNumber: yup.string().required('IC / Passport Number is required.'),
 });
+
+const filter = createFilterOptions();
 
 const PaymentFormProfile = ({ user }) => {
   const paymentForm = user?.paymentForm;
@@ -42,7 +44,11 @@ const PaymentFormProfile = ({ user }) => {
   const {
     handleSubmit,
     formState: { isDirty, isSubmitting },
+    watch,
+    setValue,
   } = methods;
+
+  const bankName = watch('bankName');
 
   const onSubmit = handleSubmit(async (data) => {
     try {
@@ -55,6 +61,17 @@ const PaymentFormProfile = ({ user }) => {
     }
   });
 
+  useEffect(() => {
+    if (bankName) {
+      if (bankName.includes('Add')) {
+        const value = bankName.split(' ').slice(1).join(' ');
+        setValue('bankName', value, { shouldDirty: true });
+      } else if (bankName !== paymentForm?.bankName) {
+        setValue('bankName', bankName, { shouldDirty: true });
+      }
+    }
+  }, [bankName, paymentForm, setValue]);
+
   return (
     <Box component={Paper} p={2}>
       <Typography variant="h6" mb={2}>
@@ -66,13 +83,52 @@ const PaymentFormProfile = ({ user }) => {
           gridTemplateColumns={{ xs: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
           gap={2}
         >
-          <RHFAutocomplete
+          {/* <RHFAutocomplete
             label="Choose a bank"
             name="bankName"
             options={banks.map((bank) => bank.bank)}
             getOptionLabel={(option) => option}
+          /> */}
+          <RHFAutocomplete
+            selectOnFocus
+            clearOnBlur
+            label="Bank Account Name"
+            name="bankName"
+            options={banks.map((item) => item.bank)}
+            getOptionLabel={(option) => option}
+            filterOptions={(options, params) => {
+              const { inputValue } = params;
+              const filtered = filter(options, params);
+
+              // Suggest the creation of a new value
+              const isExisting = options.some(
+                (option) => option.toLowerCase() === inputValue.toLowerCase()
+              );
+
+              if (inputValue !== '' && !isExisting) {
+                filtered.push(`Add ${inputValue}`);
+              }
+
+              return filtered;
+            }}
+            sx={{
+              '& .MuiInputBase-root': {
+                background: '#FFFFFF',
+                border: '1px solid #EBEBEB',
+                borderRadius: '8px',
+              },
+              '& .MuiOutlinedInput-root .MuiAutocomplete-input': {
+                padding: '5px 4px 5px 4px',
+              },
+              '& .MuiLoadingButton-root.Mui-disabled': {
+                backgroundColor: '#B0BEC5',
+                color: '#FFFFFF',
+                opacity: 1,
+                cursor: 'not-allowed',
+              },
+            }}
           />
-          {/* <RHFTextField name="bankNumber" type="number" label="Bank Account Number" /> */}
+
           <Controller
             name="bankNumber"
             render={({ field, fieldState: { error } }) => (
