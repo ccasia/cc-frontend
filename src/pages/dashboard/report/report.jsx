6fc331f1ad124242f39cad6c608e4ccfd4880eb1
Arif Sufri <arifsufri@Arifs-MacBook-Air.local>
2025-05-22 18:16:33 +0800

    Diverged

--- src/pages/dashboard/report/report.jsx
+++ src/pages/dashboard/report/report.jsx
@@ -1,6 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 
-import ReportingView from 'src/sections/report/view/reporting-view';
+import ReportingView from 'src/sections/reporting/view/reporting-view';
 
 export default function ReportPage() {
   return (
