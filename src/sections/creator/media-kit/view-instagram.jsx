f158d523d1db4070caa13ff2a142f86534928a5e
Willy Wong <willywong@x1s-Macbook-Air.local>
2025-06-24 14:23:00 +0800

    fix: Carousel thumbnail not appearing

--- src/sections/creator/media-kit/view-instagram.jsx
+++ src/sections/creator/media-kit/view-instagram.jsx
@@ -111,7 +111,7 @@ const TopContentGrid = ({ topContents, mobileCarousel }) => {
                   width: '100%',
                   transition: 'all .3s ease',
                   objectFit: 'cover',
-                  background: `linear-gradient(180deg, rgba(0, 0, 0, 0.00) 45%, rgba(0, 0, 0, 0.70) 80%), url(${content.media_url}) center/cover no-repeat`,
+                  background: `linear-gradient(180deg, rgba(0, 0, 0, 0.00) 45%, rgba(0, 0, 0, 0.70) 80%), url(${content?.media_type === 'VIDEO' ? content?.thumbnail_url : content?.media_url}) center/cover no-repeat`,
                 }}
               />
               <Box
