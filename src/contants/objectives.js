7355661fc46a5a1254e329cf4449d9b200379ab8
shenqjye93 <shenqjye93@gmail.com>
2025-08-07 17:39:40 +0800

    Added campaign objectives constants

---
+++ src/contants/objectives.js
@@ -0,0 +1,9 @@
+export const objectivesLists = [
+  'New Product Launch',
+  'New Service Launch',
+  'Increase Brand Awareness',
+  'Drive Product Awareness',
+  'Drive Service Awareness',
+  'Increase Purchase Intent',
+  'Increase Reach of Audience',
+];
