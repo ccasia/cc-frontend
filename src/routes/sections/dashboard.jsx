cf43dd36e0ee23141c8965a3243f70817482aea5
Arif Sufri <arifsufri@Arifs-MacBook-Air.local>
2025-08-14 12:38:25 +0800

    a

--- src/routes/sections/dashboard.jsx
+++ src/routes/sections/dashboard.jsx
@@ -234,7 +234,7 @@ export const dashboardRoutes = [
           {
             path: 'create/:id',
             element: (
-              <RoleBasedGuard roles={['admin']} hasContent>
+              <RoleBasedGuard roles={['admin', 'superadmin']} hasContent>
                 <CreateInvoice />
               </RoleBasedGuard>
             ),
@@ -242,7 +242,7 @@ export const dashboardRoutes = [
           {
             path: 'creator-list/:id',
             element: (
-              <RoleBasedGuard roles={['admin']} hasContent>
+              <RoleBasedGuard roles={['admin', 'superadmin']} hasContent>
                 <CreatorSelection />
               </RoleBasedGuard>
             ),
