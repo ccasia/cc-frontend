d239f793bc1ae96ca5c2680feb003aec03635e1f
sam <shenqjye93@gmail.com>
2025-12-08 12:18:22 +0800

    feat: remove products assigned when status changes to unassigned by admin, allow status update in table row when admins update status of an unassigned creator

--- src/sections/logistics/bulk-assign-view.jsx
+++ src/sections/logistics/bulk-assign-view.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import useSWR, { mutate } from 'swr';
 import { useSnackbar } from 'src/components/snackbar';
 
