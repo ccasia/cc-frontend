73937bee704983dd170ca360efe1f043f50d3883
Arif Sufri <arifsufri@Arifs-MacBook-Air.local>
2026-01-13 11:51:11 +0800

    a

--- src/sections/campaign/discover/creator/campaign-pitch-options-modal.jsx
+++ src/sections/campaign/discover/creator/campaign-pitch-options-modal.jsx
@@ -44,11 +44,26 @@ const CampaignPitchOptionsModal = ({ open, handleClose, campaign, text, video })
   const handlePitch = () => {
     // Check if user is in the target list for media kit requirement
     const targetUserIds = [
-      'cmeuvjc6b003on401rn4pw62b',
-      'cmf813vtd0000pd3psk46u4lt', 
-      'cmipdmkvd0005k43fnfgxrb4t',
-      'cmf8289xu000cpdmcj4a4fosl', 
-      'user456'
+      'cm8gvqtcv01hwph01uof2u9xu',
+      'cm49lve6i00patrd2ax5fj67h',
+      'cm4132k9p00wb54qgcrs71v0t',
+      'cmauqo8oy03ioky0157sbr2jg',
+      'cm8jxuuvy0272ph01nr0h7din',
+      'cm5b5p0zu00r2ylfpo241kqki',
+      'cmewrex4p054ipx01u5xqkqhj',
+      'cm7oe0q15005bms010ujmjb3r',
+      'cm44lei3t00si132zq87a5lan',
+      'cm9kzqz1u00ziqe01q2tsdptg',
+      'cmfb25m4r003vqn01zoe9atng',
+      'cmj9pz1n40a3hs40154b31l90',
+      'cm8mh5ic5032sph011r87rw4e',
+      'cm40womsf001k54qg4epuacmu',
+      'cm4utxiyv02mu9wevfkpyt8qj',
+      'cmfwczmov0t5rqp01aq687n4a',
+      'cmj7kdxxi05sqs401pro45vik',
+      'cmj21yl0102ghpc01xmy9zkwa',
+      'cm3pyp3vm006qm9m8qm1ep02d',
+      'cm4ey6g9401w4trd2ip0zf1et',
     ];
     const isTargetUser = targetUserIds.includes(user?.id);
     
