489f55f68c8f151cd3ebc881986899c0cc34a373
cc-esther <esther@cultcreative.asia>
2026-01-22 16:09:58 -0500

    feat(campaign): update import paths for logistic components and enhance campaign status handling

--- src/sections/client/campaign-create/campaign-create-form.jsx
+++ src/sections/client/campaign-create/campaign-create-form.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-nested-ternary */
+import dayjs from 'dayjs';
 /* eslint-disable no-unused-vars */
 import * as Yup from 'yup';
 import { pdfjs } from 'react-pdf';
@@ -36,11 +37,7 @@ import Iconify from 'src/components/iconify';
 import FormProvider from 'src/components/hook-form';
 
 import PackageCreateDialog from 'src/sections/packages/package-dialog';
-import LogisticRemarks from 'src/sections/campaign/create/steps/logistic-remarks';
-import ReservationSlots from 'src/sections/campaign/create/steps/reservation-slots';
-import CampaignLogistics from 'src/sections/campaign/create/steps/campaign-logistics';
-// Import steps from campaign creation
-import TimelineTypeModal from 'src/sections/campaign/create/steps/timeline-type-modal';
+import LogisticRemarks from 'src/sections/campaign/create/stepsV2/logistic-remarks';
 // Import steps from admin campaign creation
 import CampaignLogistics from 'src/sections/campaign/create/stepsV2/campaign-logistics';
 import ReservationSlotsV2 from 'src/sections/campaign/create/stepsV2/reservation-slots';
@@ -435,11 +432,9 @@ function ClientCampaignCreateForm({ onClose, mutate }) {
     handleSubmit,
     getValues,
     reset,
-    control,
     setValue,
     watch,
     trigger,
-    formState: { isValid, errors },
   } = methods;
 
   const values = watch();
@@ -839,7 +834,14 @@ function ClientCampaignCreateForm({ onClose, mutate }) {
       case 6:
         return (
           <NextSteps
-            onPublish={handleSubmit(onSubmit)}
+            onPublish={() => {
+              const campaignStart = getValues('campaignStartDate');
+              const campaignStatus = dayjs(campaignStart).isSame(dayjs(), 'date')
+                ? 'ACTIVE'
+                : 'SCHEDULED';
+              setStatus(campaignStatus);
+              onSubmit(campaignStatus);
+            }}
             onContinueAdditionalDetails={handleContinueAdditionalDetails}
             isLoading={isLoading}
           />
