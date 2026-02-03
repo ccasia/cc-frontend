a6a21109a08eaaf74fa6ad44ee74d4def459bd0c
Willy Wong <willywong@x1s-Macbook-Air.local>
2026-02-03 15:22:30 +0800

    feat: show campaign name and role-based titles in browser tab

--- src/pages/dashboard/campaign/admin/campaign-details.jsx
+++ src/pages/dashboard/campaign/admin/campaign-details.jsx
@@ -2,17 +2,20 @@ import { Helmet } from 'react-helmet-async';
 
 import { useParams } from 'src/routes/hooks';
 
+import { useGetCampaignById } from 'src/hooks/use-get-campaign-by-id';
+
 import CampaignDetailView from 'src/sections/campaign/discover/admin/view/campaign-detail-view';
 
 // ----------------------------------------------------------------------
 
 export default function Page() {
   const { id } = useParams();
+  const { campaign } = useGetCampaignById(id);
 
   return (
     <>
       <Helmet>
-        <title>Campaign Detail</title>
+        <title>{campaign?.name ?? 'Manage Campaign'}</title>
       </Helmet>
 
       <CampaignDetailView id={id} />
