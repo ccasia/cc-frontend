23d00b4c79f53c0098d466b4e9f9f2466a09c278
cc-esther <esther@cultcreative.asia>
2026-02-11 15:24:08 -0500

    fix (campaign-creator): fullWidth of campaign modal on discover page

--- src/sections/campaign/discover/creator/campaign-modal.jsx
+++ src/sections/campaign/discover/creator/campaign-modal.jsx
@@ -315,7 +315,7 @@ const CampaignModal = ({
   };
 
   return (
-    <Dialog open={open} onClose={handleClose} maxWidth="md" scroll="body">
+    <Dialog open={open} onClose={handleClose} fullWidth maxWidth="md" scroll="body">
       <DialogContent sx={{ p: 0 }}>
         {/* Campaign image */}
         <Box
