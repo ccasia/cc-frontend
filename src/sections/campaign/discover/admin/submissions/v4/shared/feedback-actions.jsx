cbe6a4d3a203797a740e1d734acd990d9948c598
Willy Wong <willywong@x1s-Macbook-Air.local>
2026-03-11 13:10:40 +0800

    a

--- src/sections/campaign/discover/admin/submissions/v4/shared/feedback-actions.jsx
+++ src/sections/campaign/discover/admin/submissions/v4/shared/feedback-actions.jsx
@@ -18,7 +18,7 @@ import Iconify from 'src/components/iconify';
 
 
 import ConfirmDialogV2 from 'src/components/custom-dialog/confirm-dialog-v2';
-import VideoSubmissionModal from 'src/sections/campaign/manage-creator/v4/submissions/VideoSubmissionModal';
+import VideoSubmissionModal from 'src/sections/campaign/manage-creator/v4/submissions/AdminVideoSubmissionModal';
 import AdminFeedbackPanel from 'src/sections/campaign/manage-creator/v4/submissions/admin-feedback-modal';
 
 import { options_changes } from '../constants';
