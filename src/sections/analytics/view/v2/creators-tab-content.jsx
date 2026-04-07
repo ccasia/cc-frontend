042b70cbe46e0b1f6c10dbff95ddbe1cd69eed46
afiq <afiq@nexea.co>
2026-04-08 00:18:56 +0800

    a

--- src/sections/analytics/view/v2/creators-tab-content.jsx
+++ src/sections/analytics/view/v2/creators-tab-content.jsx
@@ -8,7 +8,6 @@ import PitchRateChart from './creators/pitch-rate-chart';
 import CreatorNpsChart from './creators/creator-nps-chart';
 import CreatorGrowthChart from './creators/creator-growth-chart';
 import ResponseTimeCharts from './creators/response-time-charts';
-import CreatorCountryMapChart from './creators/creator-country-map-chart';
 import ActivationRateChart from './creators/activation-rate-chart';
 import CreatorEarningsChart from './admins/creator-earnings-chart';
 import TimeToActivationChart from './creators/time-to-activation-chart';
